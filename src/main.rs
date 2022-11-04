051abdedb27f3d830173954324be04db45fa58df
Ben Doerry <ben@doerry.uk>
2022-11-04 21:16:15 +0000

    Don't hardcode address into server
    This might allow us to use random ports for tests.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run()?.await
+    run("127.0.0.1:8000")?.await
 }
