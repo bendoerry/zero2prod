2810312a5b9f3b8e9f146b5753d5e1be21cb367d
Ben Doerry <ben@doerry.uk>
2022-11-13 17:32:23 +0000

    Use `sqlx` for DB integration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,6 +7,14 @@ if ! [ -x "$(command -v psql)" ]; then
   exit 1
 fi
 
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install sqlx-cli --no-default-features --features postgres,rustls"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
@@ -35,3 +43,5 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+
+sqlx database create
