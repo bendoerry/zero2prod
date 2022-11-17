dbf791e8660a3ca08e524fb468bd68630f09e777
Ben Doerry <ben@doerry.uk>
2022-11-17 18:28:57 +0000

    Add a tracing span to subscription requests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,24 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
         "request_id {request_id} - Adding '{}' '{}' as a new subscriber",
         form.email,
         form.name
     );
-    log::info!("request_id {request_id} - Saving new subscriber details in the database");
+    tracing::info!("request_id {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -31,12 +43,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("request_id {request_id} - New subscriber details have been saved");
+            tracing::info!("request_id {request_id} - New subscriber details have been saved");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("request_id {request_id} - Failed to execute query: {e:?}");
+            tracing::error!("request_id {request_id} - Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
+
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
 }
