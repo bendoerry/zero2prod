00f1a1acfc72e69e0ed0e15dc1234bf7aaa360c4
Ben Doerry <ben@doerry.uk>
2022-11-18 19:35:23 +0000

    Make host configurable

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // No longer async, given that we don't actually try to connect!
     let connection_pool =
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
     let listener = TcpListener::bind(address)?;
 
