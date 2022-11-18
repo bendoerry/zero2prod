72ebbd922e6e8a22f083b51e0c39b64995859d0f
Ben Doerry <ben@doerry.uk>
2022-11-18 22:54:00 +0000

    Connect to DB with SSL

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,6 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -16,6 +16,8 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    // Determine if we demand the connection to be encrypted or not
+    pub require_ssl: bool,
 }
 
 #[derive(serde::Deserialize)]
@@ -26,18 +28,24 @@ pub struct ApplicationSettings {
 }
 
 impl DatabaseSettings {
-    // Renamed from `connection_string`
     pub fn with_db(&self) -> PgConnectOptions {
         self.without_db().database(&self.database_name)
     }
 
-    // Renamed from `connection_string_without_db`
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 }
 
