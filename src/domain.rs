310fe672ee5a7bf6849b4e43227ec3e6e30d1a6c
Ben Doerry <ben@doerry.uk>
2022-11-19 14:29:58 +0000

    Return `Result` from parse
    This allows us to handle errors gracefully

--- src/domain.rs
+++ src/domain.rs
@@ -11,7 +11,7 @@ impl SubscriberName {
     /// Returns an instance of `SubscriberName` if the input satisfies all
     /// our validation constraints on subscriber names.
     /// It panics otherwise.
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         // `.trim()` returns a view over the input `s` without trailing
         // whitespace-like characters.
         // `.is_empty` checks if the view contains any character.
@@ -34,7 +34,7 @@ impl SubscriberName {
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
             panic!("{s} is not a valid subscriber name.")
         } else {
-            Self(s)
+            Ok(Self(s))
         }
     }
 }
