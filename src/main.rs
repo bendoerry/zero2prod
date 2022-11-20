99bf8d63275d54e2f068d56eaad18e52ac404353
Ben Doerry <ben@doerry.uk>
2022-11-20 17:17:16 +0000

    Create email client from config on launch

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -12,8 +13,15 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        // `connect_lazy_with` instead of `connect_lazy`
         .connect_lazy_with(configuration.database.with_db());
+
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
@@ -21,5 +29,5 @@ async fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
