8c5460fe28bf4b3d5913fadd681f019f03965347
Ben Doerry <ben@doerry.uk>
2022-11-22 21:36:31 +0000

    Return subscriber id from `insert_subscriber`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -103,13 +103,15 @@ pub async fn send_confirmation_email(
 pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
-) -> Result<(), sqlx::Error> {
+) -> Result<Uuid, sqlx::Error> {
+    let subscriber_id = Uuid::new_v4();
+
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
         VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
-        Uuid::new_v4(),
+        subscriber_id,
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now(),
@@ -120,7 +122,8 @@ pub async fn insert_subscriber(
         tracing::error!("Failed to execute query: {e:?}");
         e
     })?;
-    Ok(())
+
+    Ok(subscriber_id)
 }
 
 /// Generate a random 25-characters-long case-sensitive subscription token.
