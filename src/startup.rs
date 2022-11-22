cf6d824decf25d9811994a59192f7e8955abc729
Ben Doerry <ben@doerry.uk>
2022-11-22 20:47:06 +0000

    Construct confirmation link using server host

--- src/startup.rs
+++ src/startup.rs
@@ -1,22 +1,21 @@
-use crate::configuration::{DatabaseSettings, Settings};
-use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use reqwest::Url;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-// A new type to hold the newly built server and its port
+use crate::configuration::{DatabaseSettings, Settings};
+use crate::email_client::EmailClient;
+use crate::routes::{confirm, health_check, subscribe};
+
 pub struct Application {
     port: u16,
     server: Server,
 }
 
 impl Application {
-    // We have converted the `build` function into a constructor for
-    // `Application`.
     pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
@@ -43,8 +42,9 @@ impl Application {
 
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
+        let base_url = configuration.application.url().expect("Invalid host url.");
 
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(listener, connection_pool, email_client, base_url)?;
 
         // We "save" the bound port in one of `Application`'s fields
         Ok(Self { port, server })
@@ -67,13 +67,22 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .connect_lazy_with(configuration.with_db())
 }
 
+// We need to define a wrapper type in order to retrieve the URL
+// in the `subscribe` handler.
+// Retrieval from the context, in actix-web, is type-based: using
+// a raw `Url` would expose us to conflicts.
+pub struct ApplicationBaseUrl(pub Url);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: Url,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
+    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -82,6 +91,7 @@ pub fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
