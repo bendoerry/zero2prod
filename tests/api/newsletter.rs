95293cda88f3bbdf42ba752debf055ecc1a596c8
Ben Doerry <ben@doerry.uk>
2022-11-25 23:32:40 +0000

    Test that we email confirmed subscribers

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -11,15 +11,11 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
 
     Mock::given(any())
         .respond_with(ResponseTemplate::new(200))
-        // We assert that no request is fired at Postmark!
         .expect(0)
         .mount(&app.email_server)
         .await;
 
     // Act
-
-    // A sketch of the newsletter payload structure.
-    // We might change it later on.
     let newsletter_request_body = serde_json::json!({
         "title": "Newsletter title",
         "content": {
@@ -37,6 +33,39 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     // Assert
     assert_eq!(response.status().as_u16(), 200);
     // Mock verifies on Drop that we haven't sent the newsletter email
+}
+
+#[tokio::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    // Arrange
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+             "text": "Newsletter body as plain text",
+             "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+    // Mock verifies on Drop that we have sent the newsletter email
 }
 
 /// Use the public API of the application under test to create
