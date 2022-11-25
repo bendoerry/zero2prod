7a2d37475453881d43a2ed1cfb17769cbb4b8987
Ben Doerry <ben@doerry.uk>
2022-11-25 23:53:02 +0000

    Retrieve confirmed subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,11 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
+pub async fn publish_newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
     HttpResponse::Ok().finish()
 }
 
@@ -14,4 +19,25 @@ pub struct BodyData {
 pub struct Content {
     html: String,
     text: String,
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rows)
 }
