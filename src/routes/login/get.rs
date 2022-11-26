7b88a01dce5e3d9cc3363b03754f161b88efaa79
Ben Doerry <ben@doerry.uk>
2022-11-26 23:55:43 +0000

    Extract error from cookies

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,11 @@
 use actix_web::http::header::ContentType;
-use actix_web::HttpResponse;
+use actix_web::{HttpRequest, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
-    let error_html: String = todo!();
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
+        None => "".into(),
+        Some(cookie) => format!("<p><i>{}</i></p>", cookie.value()),
+    };
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
