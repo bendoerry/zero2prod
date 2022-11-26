4e3d6fbc361acc1c8c77b8c6659951c014b353d6
Ben Doerry <ben@doerry.uk>
2022-11-26 12:06:39 +0000

    Add test user to DB in tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -47,11 +47,12 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
+
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
-            // Random credentials!
-            // `reqwest` does all the encoding/formatting heavy-lifting for us.
-            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
+            // No longer randomly generated on the spot!
+            .basic_auth(username, Some(password))
             .json(&body)
             .send()
             .await
@@ -85,6 +86,14 @@ impl TestApp {
 
         ConfirmationLinks { html, plain_text }
     }
+
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
+            .fetch_one(&self.db_pool)
+            .await
+            .expect("Failed to create test users.");
+        (row.username, row.password)
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
@@ -116,12 +125,16 @@ pub async fn spawn_app() -> TestApp {
     let port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
-    TestApp {
+    let test_app = TestApp {
         address: format!("http://127.0.0.1:{port}"),
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         port,
-    }
+    };
+
+    add_test_user(&test_app.db_pool).await;
+
+    test_app
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
@@ -144,4 +157,19 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to migrate the database");
 
     connection_pool
+}
+
+async fn add_test_user(pool: &PgPool) {
+    sqlx::query!(
+        r#"
+        INSERT INTO users (user_id, username, password)
+        VALUES ($1, $2, $3)
+        "#,
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+    .execute(pool)
+    .await
+    .expect("Failed to create test users.");
 }
