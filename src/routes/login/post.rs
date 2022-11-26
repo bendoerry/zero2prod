9d129d7509809f806e0cf7dedfe9ac04bfff7340
Ben Doerry <ben@doerry.uk>
2022-11-26 18:47:01 +0000

    Handle auth errors on login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,9 +1,11 @@
 use actix_web::http::header::LOCATION;
-use actix_web::{web, HttpResponse};
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
 use secrecy::Secret;
 use sqlx::PgPool;
 
-use crate::authentication::{validate_credentials, Credentials};
+use crate::authentication::{validate_credentials, AuthError, Credentials};
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,20 +17,48 @@ pub struct FormData {
     skip(form, pool)
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
-pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
     };
     tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
 
-    match validate_credentials(credentials, &pool).await {
-        Ok(user_id) => {
-            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
-            HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/"))
-                .finish()
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+    Ok(HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed.")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong.")]
+    UnexpectedError(#[source] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::UnexpectedError(_) => StatusCode::UNAUTHORIZED,
         }
-        Err(_) => todo!(),
     }
 }
