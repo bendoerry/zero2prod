8adadbdab5cac6fb7bd57f17213c5cf045a7ff3b
Ben Doerry <ben@doerry.uk>
2022-11-27 20:56:39 +0000

    Retrieve session from request

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,12 +1,17 @@
-use actix_web::{
-    body::MessageBody,
-    dev::{ServiceRequest, ServiceResponse},
-};
+use actix_web::body::MessageBody;
+use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::FromRequest;
 use actix_web_lab::middleware::Next;
+
+use crate::session_state::TypedSession;
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
     next: Next<impl MessageBody>,
 ) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
+    let session = {
+        let (http_request, payload) = req.parts_mut();
+        TypedSession::from_request(http_request, payload).await
+    }?;
     todo!()
 }
