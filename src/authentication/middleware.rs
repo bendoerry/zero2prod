2ed3ebc58eb8f26bba09c3d1013fce4c705f8d6d
Ben Doerry <ben@doerry.uk>
2022-11-27 21:02:17 +0000

    Reject requests without user id in session

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,9 +1,11 @@
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::error::InternalError;
 use actix_web::FromRequest;
 use actix_web_lab::middleware::Next;
 
 use crate::session_state::TypedSession;
+use crate::utils::{e500, see_other};
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
@@ -13,5 +15,13 @@ pub async fn reject_anonymous_users(
         let (http_request, payload) = req.parts_mut();
         TypedSession::from_request(http_request, payload).await
     }?;
-    todo!()
+
+    match session.get_user_id().map_err(e500)? {
+        Some(_) => next.call(req).await,
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged.");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
