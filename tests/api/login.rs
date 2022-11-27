f1c2706427dbed50a7616ceacc1bd3c4f2f7bec4
Ben Doerry <ben@doerry.uk>
2022-11-27 00:25:38 +0000

    Don't assert cookie content in test

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -14,8 +14,7 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     // Assert
     assert_is_redirect_to(&response, "/login");
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
-    assert_eq!(flash_cookie.value(), "Authentication failed.");
+    // No longer asserting facts related to cookies
 
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_login_html().await;
