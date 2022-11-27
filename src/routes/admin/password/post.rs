ed3a35f4decc790ff7c99e1a722dad2e89a84221
Ben Doerry <ben@doerry.uk>
2022-11-27 17:41:54 +0000

    Retrieve username using user id

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,9 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
+use crate::routes::admin::dashboard::get_username;
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 
@@ -14,14 +16,15 @@ pub struct FormData {
 
 pub async fn change_password(
     form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
     session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+    if user_id.is_none() {
         return Ok(see_other("/login"));
     }
+    let user_id = user_id.unwrap();
 
-    // `Secret<String>` does not implement `Eq`,
-    // therefore we need to compare the underlying `String`.
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "You entered two different new passwords - the field values must match.",
@@ -29,6 +32,8 @@ pub async fn change_password(
         .send();
         return Ok(see_other("/admin/password"));
     }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
 
     todo!()
 }
