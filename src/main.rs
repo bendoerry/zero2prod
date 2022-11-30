0a817b03419eb4c6bd7de76a927fc3b6deecec0e
Ben Doerry <ben@doerry.uk>
2022-11-30 00:20:47 +0000

    Log task completion/failures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fmt::{Debug, Display};
+
+use tokio::task::JoinError;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::issue_delivery_worker::run_worker_until_stopped;
 use zero2prod::startup::Application;
@@ -16,9 +20,31 @@ async fn main() -> anyhow::Result<()> {
     let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
     tokio::select! {
-        _ = application_task => {},
-        _ = worker_task => {},
+        o = application_task => report_exit("API", o),
+        o = worker_task => report_exit("Background worker", o),
     }
 
     Ok(())
+}
+
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{task_name} has exited.")
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{task_name} failed.",
+            )
+        }
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "'{task_name}' task failed to complete.",
+            )
+        }
+    }
 }
