62d11558180b6a167e59a113852a0ac85861b48b
Ben Doerry <ben@doerry.uk>
2022-11-30 00:15:46 +0000

    Spawn tasks in separate threads

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ async fn main() -> anyhow::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let application = Application::build(configuration.clone())
-        .await?
-        .run_until_stopped();
-    let worker = run_worker_until_stopped(configuration);
+    let application = Application::build(configuration.clone()).await?;
+
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
     tokio::select! {
-        _ = application => {},
-        _ = worker => {},
+        _ = application_task => {},
+        _ = worker_task => {},
     }
 
     Ok(())
